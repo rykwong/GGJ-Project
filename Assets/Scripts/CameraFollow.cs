6aee570e882602ea9b86a252e9fd80085272796c
Gokul Krishna <gokris165@gmail.com>
2021-01-30 12:01:03 -0800

    added movement and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public Transform target;
+
+   public float smoothSpeed = 0.125f; 
+   public Vector3 offset;
+
+   void LateUpdate()
+   {
+   		//transform.position = target.position + offset;
+   		Vector3 desiredPosition = target.position + offset;
+   		transform.position = desiredPosition;
+
+   		transform.LookAt(target);
+   }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public CharacterController2D controller;
+	public float runSpeed = 40f;
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    	horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    	if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+    	{
+    		jump = true;
+    	}
+    	if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+    	{
+    		crouch = true;
+    	}
+    	else if(Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+    	{
+    		crouch = false;
+    	}
+    }
+
+    void FixedUpdate()
+    {
+    	// Move Character
+    	controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+    	jump = false;
+    }
+}

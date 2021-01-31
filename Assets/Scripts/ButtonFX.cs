23c2fb76ee2c1e3e4933dbae208d1f732e8f64b0
Gokul Krishna <gokris165@gmail.com>
2021-01-30 19:07:28 -0800

    added menu

---
+++ Assets/Scripts/ButtonFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    public void HoverSound()
+    {
+    	source.PlayOneShot(hoverSound);
+    }
+    public void ClickSound()
+    {
+    	source.PlayOneShot(clickSound);
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+    	StartCoroutine(ButtonDelay());
+    }
+    IEnumerator ButtonDelay()
+	{
+   		Debug.Log(Time.time);
+	   	yield return new WaitForSeconds(0.45f);
+    	Debug.Log(Time.time);
+
+    	// This line will be executed after 10 seconds passed
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+    public void QuitGame()
+    {
+    	Debug.Log("QUIT!");
+    	Application.Quit();
+    }
+}
